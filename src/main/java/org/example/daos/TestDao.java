c17845f93c6b657f9366996382237eef240ef991
laurenobrien <laurenob1@live.co.uk>
2025-06-23 11:56:49 +0100

    commit message

--- src/main/java/org/example/daos/TestDao.java
+++ src/main/java/org/example/daos/TestDao.java
@@ -20,7 +20,8 @@ public class TestDao {
                     "SHOW DATABASES;");
             long end = System.currentTimeMillis();
 
-            System.out.println("Total time to execute query in milliseconds: " + (end - start));
+            System.out.println("Total time to execute " +
+                    "query in milliseconds: " + (end - start));
 
             while (resultSet.next()) {
                 databases.add(resultSet.getString("Database"));
